245682b83f2484df47883b2d8acb48198f899de2
Freezler <r.l.devries@live.nl>
2024-04-21 08:16:12 +0200

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,20 @@ const config = {
       inter: ["Inter"],
       cursive: ["cursive"]
 
-    }
-  }
+    },
+    animation: {
+      slide: "slide 2.5s linear infinite",
+    },
+    keyframes: {
+      slide: {
+        "0%": { transform: "translateY(100%)", opacity: 0.1 },
+        "15%": { transform: "translateY(0)", opacity: 1 },
+        "30%": { transform: "translateY(0)", opacity: 1 },
+        "45%": { transform: "translateY(-100%)", opacity: 1 },
+        "100%": { transform: "translateY(-100%)", opacity: 0.1 },
+      },
+    },
+  },
 };
 
 export default config;
